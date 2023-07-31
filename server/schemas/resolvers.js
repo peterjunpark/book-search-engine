9a8bfdb341e24177d32818645553d43fa4f807ce
Peter Park <peterjpark@pm.me>
2023-07-30 22:30:33 -0400

    Add gql queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,22 @@
 const { Book, User } = require("../models");
+const { AuthenticationError } = require("@apollo/server");
+const { SignToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    books: "",
-    book: "",
-    users: "",
-    user: "",
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user_id }).select(
+          "-__v -password" // Don't get password.
+        );
+
+        return userData;
+      }
+
+      throw new AuthenticationError("Must be logged in.");
+    },
   },
+  Mutation: {},
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,22 +2,41 @@ const { gql } = require("@apollo/server");
 
 const typeDefs = gql`
   type Book {
-    bookId: ID
-    title: String
+    bookId: ID!
+    title: String!
     description: String
     authors: [String]
     link: String
+    image: String
   }
   type User {
-    _id: ID
-    username: String
+    _id: ID!
+    username: String!
     email: String
     password: String
     savedBooks: [Book]
+    bookCount: Int
   }
   type Auth {
-    token: String
+    token: ID!
     user: User
+  }
+  input BookInput {
+    bookId: String!
+    title: String!
+    description: String
+    authors: [String]
+    link: String
+    image: String
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
   }
 `;
 
