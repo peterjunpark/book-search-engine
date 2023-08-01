5d36bf682974a2d3ccad5cdb761208c421d3a482
Peter Park <peterjpark@pm.me>
2023-08-01 00:42:22 -0400

    Fix graphql uri and cleanup ReactDOM render syntax

--- client/src/index.js
+++ client/src/index.js
@@ -6,15 +6,15 @@ import App from "./App";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "/graphql",
   cache: new InMemoryCache(),
 });
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
+ReactDOM.render(
   <ApolloProvider client={client}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </ApolloProvider>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
