05c214090589254d8b2bf3df218b7a2c5e7ae4ca
Peter Park <peterjpark@pm.me>
2023-08-01 00:51:36 -0400

    Move Apollo Client from index.js to App.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,18 +3,10 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "/graphql",
-  cache: new InMemoryCache(),
-});
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ApolloProvider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
